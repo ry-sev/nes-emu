a71541d06a5e065ed2f28cdd0c975d65b7c0acdf
ry-sev <rickyseverino@protonmail.com>
2022-04-13 22:57:30 -0400

    Main: Remove code that is not in Bus

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,7 @@ int main()
     bus.connect_to_cpu(&cpu);
     cpu.connect_to_bus(&bus);
 
-    std::stringstream ss;
-    ss << "A2 0A 8E 00 00 A2 03 8E 01 00 AC 00 00 A9 00 18 6D 01 00 88 D0 FA 8D 02 00 EA EA EA";
-    u16 offset = 0x8000;
-    while (!ss.eof()) {
-        std::string b;
-        ss >> b;
-        bus.ram[offset++] = (u8)std::stoul(b, nullptr, 16);
-    }
+    bus.load_program_from_string("A2 0A 8E 00 00 A2 03 8E 01 00 AC 00 00 A9 00 18 6D 01 00 88 D0 FA 8D 02 00 EA EA EA");
 
     bus.write(0xFFFC, 0x00);
     bus.write(0xFFFD, 0x80);
