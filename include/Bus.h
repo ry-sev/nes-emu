1a1a147ca203cab47d37834f82863266f0333aef
ry-sev <rickyseverino@protonmail.com>
2022-04-13 22:56:59 -0400

    Bus: Add function to load program from string

--- include/Bus.h
+++ include/Bus.h
@@ -15,6 +15,7 @@ public:
     u8 ram[RAM_SIZE];
 
     void connect_to_cpu(CPU6502* cpu) { m_cpu = cpu; };
+    void load_program_from_string(const char* program);
     u8 read(u16 address);
     void write(u16 address, u8 value);
 
--- src/Bus.cpp
+++ src/Bus.cpp
@@ -1,9 +1,22 @@
-#include <cstring>
 #include "Bus.h"
+#include <cstring>
+#include <sstream>
 
 Bus::Bus()
 {
     memset(ram, 0x0000, RAM_SIZE);
+}
+
+void Bus::load_program_from_string(const char* program)
+{
+    std::stringstream stream;
+    stream << program;
+    u16 offset = 0x8000;
+    while (!stream.eof()) {
+        std::string b;
+        stream >> b;
+        ram[offset++] = (u8)std::stoul(b, nullptr, 16);
+    }
 }
 
 u8 Bus::read(u16 address)
