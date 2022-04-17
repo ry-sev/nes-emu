3291ab6ab1cc9b8d32db316732b013ebb6e12a6f
ry-sev <rickyseverino@protonmail.com>
2022-04-17 15:38:49 -0400

    MenuWidget: Show framerate info

--- src/GUI/MenuWidget.cpp
+++ src/GUI/MenuWidget.cpp
@@ -8,7 +8,11 @@ MenuWidget::MenuWidget()
 void MenuWidget::render()
 {
     if (ImGui::BeginMainMenuBar()) {
-        ImGui::Button("Reset");
+        ImGui::Text(
+            "Application average %.3f ms/frame (%.1f FPS)",
+            1000.0f / ImGui::GetIO().Framerate,
+            ImGui::GetIO().Framerate
+        );
         ImGui::EndMainMenuBar();
     }
 }
